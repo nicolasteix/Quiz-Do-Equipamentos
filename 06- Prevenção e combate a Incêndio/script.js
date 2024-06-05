81b0b3c397ddb7b7dc324d5a607665e2ea826c05
Nicolas Figueira <162911789+nicolasteix@users.noreply.github.com>
2024-06-05 12:38:24 -0300

    Update script.js

--- 06- Prevenção e combate a Incêndio/script.js
+++ 06- Prevenção e combate a Incêndio/script.js
@@ -49,7 +49,16 @@ const flashcards = [
         answer: "Devido a falhas na prevenção e/ou descuido humano"
     },
 ];
+// Função para embaralhar um array
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]]; // Troca os elementos
+  }
+}
 
+// Embaralha o array de flashcards
+shuffleArray(flashcards);
 const flashcardsContainer = document.getElementById('flashcards');
 const questionElement = document.getElementById('question-text');
 const answerElement = document.getElementById('answer');
