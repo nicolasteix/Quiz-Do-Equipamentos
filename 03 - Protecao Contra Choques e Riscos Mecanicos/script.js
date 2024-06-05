f7868f4edd33db53cd8fd8ca7aa05b1d244422b0
Nicolas Figueira <162911789+nicolasteix@users.noreply.github.com>
2024-06-05 12:37:30 -0300

    Update script.js

--- 03 - Protecao Contra Choques e Riscos Mecanicos/script.js
+++ 03 - Protecao Contra Choques e Riscos Mecanicos/script.js
@@ -88,6 +88,16 @@ const flashcards = [
           "QUÍMICOS, FÍSICOS, BIOLÓGICOS, ERGONÔMICOS e ACIDENTAIS.",
       },
     ];
+// Função para embaralhar um array
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]]; // Troca os elementos
+  }
+}
+
+// Embaralha o array de flashcards
+shuffleArray(flashcards);
 
 const flashcardsContainer = document.getElementById('flashcards');
 const questionElement = document.getElementById('question-text');
