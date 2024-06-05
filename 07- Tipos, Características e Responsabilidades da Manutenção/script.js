40b989f5d6c3bb4eb4fb8d45a79c516437da4eb1
Nicolas Figueira <162911789+nicolasteix@users.noreply.github.com>
2024-06-05 12:38:52 -0300

    Update script.js

--- 07- Tipos, Características e Responsabilidades da Manutenção/script.js
+++ 07- Tipos, Características e Responsabilidades da Manutenção/script.js
@@ -74,7 +74,16 @@ const flashcards = [
         answer: "restauração e inspeção de componentes, garantia das funções do equipamento durante a produção, melhor ritmo de trabalho e menores chances de erros. As desvantagens incluem a possibilidade de substituição de componentes ainda em condições de uso e a ocorrência de falhas entre os períodos planejados"
     },
 ];
+// Função para embaralhar um array
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]]; // Troca os elementos
+  }
+}
 
+// Embaralha o array de flashcards
+shuffleArray(flashcards);
 const flashcardsContainer = document.getElementById('flashcards');
 const questionElement = document.getElementById('question-text');
 const answerElement = document.getElementById('answer');
